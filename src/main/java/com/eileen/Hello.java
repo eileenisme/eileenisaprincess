8482ab93732e8764c0fe8dbfadc9f3b1be04225b
Chi-Hsien Lin <chi-hsienlin@Chi-HsiendeMacBook-Pro.local>
2021-01-20 19:58:59 +0800

    Initial commit

---
+++ src/main/java/com/eileen/Hello.java
@@ -0,0 +1,12 @@
+package com.eileen;
+
+public class Hello {
+    public static void main(String[]arg) {
+        //System.out.println("love you");
+        Person p = new Person();
+        p.hello();
+;        }
+    }
+
+
+
